899ce923011a80a1be562248ec73e045d9426963
Corey Alexander <coreyja@gmail.com>
2023-02-08 19:59:31 -0500

    We can now parse Params and make structs for them

--- openapi-gen-core/src/lib.rs
+++ openapi-gen-core/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use openapiv3::*;
 use proc_macro2::TokenStream;
 use quote::format_ident;
@@ -72,12 +74,103 @@ trait IntoOperationMod {
     fn into_op_mod(self, verb: &str) -> syn::ItemMod;
 }
 
+trait IntoType {
+    fn into_type(&self, new_structs: &mut Vec<ItemStruct>, name: &str, count: usize)
+        -> TokenStream;
+}
+
+impl IntoType for Schema {
+    fn into_type(
+        &self,
+        new_structs: &mut Vec<ItemStruct>,
+        name: &str,
+        count: usize,
+    ) -> TokenStream {
+        match &self.schema_kind {
+            SchemaKind::Type(t) => into_type(t, new_structs, name, count),
+            SchemaKind::OneOf { .. } => todo!("Generate an enum from the possible schemas"),
+            SchemaKind::AllOf { .. } => {
+                todo!("IDK... Try to make a struct thats the union of all the schemas?")
+            }
+            SchemaKind::AnyOf { .. } => todo!("Is this the same as oneOf?"),
+            SchemaKind::Not { .. } => {
+                todo!("Dont think we can really support a NOT schema.... Just skip it?")
+            }
+            SchemaKind::Any(_) => todo!("Untyped schema... Just skip it?"),
+        }
+    }
+}
+
+fn into_type(t: &Type, new_structs: &mut Vec<ItemStruct>, name: &str, count: usize) -> TokenStream {
+    match t {
+        Type::String(_) => quote::quote!(String),
+        Type::Number(_) => quote::quote!(f64),
+        Type::Integer(_) => quote::quote!(i64),
+        Type::Object(o) => {
+            let ident = if count == 0 {
+                name.to_string()
+            } else {
+                format!("{name}_{count}")
+            };
+            let ident = format_ident!("{ident}");
+            let mut s: ItemStruct = parse_quote! {
+              pub struct #ident {}
+            };
+
+            if let Fields::Named(ref mut fields) = s.fields {
+                if o.additional_properties.is_some() {
+                    todo!("We dont support additional properties yet")
+                }
+
+                for (key, value) in o.properties.iter() {
+                    let s = value.as_item().unwrap();
+
+                    let ty = s.into_type(new_structs, name, count + 1);
+
+                    let key = format_ident!("{key}");
+
+                    fields.named.push(
+                        syn::Field::parse_named
+                            .parse2(quote::quote! { pub #key: #ty })
+                            .unwrap(),
+                    );
+                }
+            } else {
+                panic!("This should always be named cause we just made the struct")
+            };
+
+            new_structs.push(s);
+
+            quote::quote!(#ident)
+        }
+        Type::Array(a) => {
+            let sample = a.items.iter().next();
+            let ty = match sample {
+                Some(s) => {
+                    let s = s.as_item().unwrap();
+
+                    s.into_type(new_structs, name, count + 1)
+                }
+                None => todo!("What do we do with empty arrays?"),
+            };
+
+            quote::quote!(Vec<#ty>)
+        }
+        Type::Boolean {} => quote::quote!(bool),
+    }
+}
+
 impl IntoOperationMod for Operation {
     fn into_op_mod(self, verb: &str) -> syn::ItemMod {
         let mut verb = verb.to_string();
         make_ascii_titlecase(&mut verb);
 
         let ident = format_ident!("{verb}");
+
+        let mut operation_mod: ItemMod = parse_quote! {
+          pub mod #ident {}
+        };
+        let content = &mut operation_mod.content.as_mut().unwrap().1;
 
         let request_struct: ItemStruct = parse_quote! {
           pub struct Request {}
@@ -87,7 +180,8 @@ impl IntoOperationMod for Operation {
           #[doc="Test this Response"]
           pub enum Response { }
         };
-        let mut response_structs: Vec<ItemStruct> = vec![];
+
+        let mut structs: Vec<ItemStruct> = vec![];
         for (status_code, resp) in self.responses.responses {
             let resp = resp.as_item().unwrap();
             let variant_ident = format_ident!("_{status_code}");
@@ -99,78 +193,128 @@ impl IntoOperationMod for Operation {
             let example_value = example_value.unwrap().value.unwrap();
 
             let struct_ident = format!("Response{status_code}");
-            let ty = type_for(&example_value, &mut response_structs, &struct_ident, 0);
+            let ty = type_for(&example_value, &mut structs, &struct_ident, 0);
 
             response_enum.variants.push(parse_quote! {
               #variant_ident(#ty)
             });
         }
 
-        let mut x: ItemMod = parse_quote! {
-          pub mod #ident {
-            #request_struct
-
-            #response_enum
-          }
+        let mut param_struct: ItemStruct = parse_quote! {
+          pub struct QueryParams {}
         };
-        let content = &mut x.content.as_mut().unwrap().1;
+        if let Fields::Named(ref mut fields) = param_struct.fields {
+            for param in self.parameters {
+                let param = param.into_item().unwrap();
+                match param {
+                    Parameter::Query { parameter_data, .. } => {
+                        let name = parameter_data.name;
+                        let ident = format_ident!("{name}");
 
-        for i in response_structs {
+                        if let ParameterSchemaOrContent::Schema(s) = parameter_data.format {
+                            let s = s.into_item().unwrap();
+
+                            let struct_name = format!("QueryParams_Schema");
+                            let ty = s.into_type(&mut structs, &struct_name, 0);
+
+                            fields.named.push(
+                                syn::Field::parse_named
+                                    .parse2(quote::quote! { pub #ident: #ty })
+                                    .unwrap(),
+                            );
+                        } else {
+                            todo!("Need to handle cases where we have the nested content instead of a schema")
+                        }
+                    }
+                    Parameter::Header { .. } => todo!(),
+                    Parameter::Path { .. } => todo!(),
+                    Parameter::Cookie { .. } => todo!(),
+                };
+            }
+        } else {
+            panic!("This should always be named cause we just made the struct")
+        };
+
+        content.push(param_struct.into());
+
+        content.push(request_struct.into());
+        content.push(response_enum.into());
+
+        for i in structs {
             content.push(i.into())
         }
 
-        x
+        operation_mod
     }
 }
 
-fn type_for(value: &Value, structs: &mut Vec<ItemStruct>, name: &str, count: usize) -> TokenStream {
+fn type_for_value(value: &Value) -> Type {
     match value {
-        Value::Null => quote::quote!(()),
-        Value::Bool(_) => quote::quote!(bool),
-        // TODO: i64 isn't quite right here, we probably want to mess with this
-        Value::Number(_) => quote::quote!(i64),
-        Value::String(_) => quote::quote!(String),
+        Value::Null => todo!("What do we do with nulls?"),
+        Value::Bool(_) => Type::Boolean {},
+        Value::Number(n) => {
+            if n.is_f64() {
+                Type::Number(Default::default())
+            } else {
+                Type::Integer(Default::default())
+            }
+        }
+        Value::String(_) => Type::String(Default::default()),
         Value::Array(a) => {
             let sample = a.iter().next();
             let ty = match sample {
-                Some(v) => type_for(v, structs, name, count),
+                Some(v) => type_for_value(v),
                 None => todo!("What do we do with empty arrays?"),
             };
 
-            quote::quote!(Vec<#ty>)
+            let s = Schema {
+                schema_data: Default::default(),
+                schema_kind: SchemaKind::Type(ty),
+            };
+            let s = Box::new(s);
+            let s = ReferenceOr::Item(s);
+
+            Type::Array(ArrayType {
+                items: s.into(),
+                min_items: None,
+                max_items: None,
+                unique_items: false,
+            })
         }
         Value::Object(o) => {
-            let ident = if count == 0 {
-                name.to_string()
-            } else {
-                format!("{name}_{count}")
-            };
-            let ident = format_ident!("{ident}");
-            let mut s: ItemStruct = parse_quote! {
-              pub struct #ident {}
-            };
+            let mut properties = HashMap::<String, _>::new();
+            for (key, value) in o.iter() {
+                let ty = type_for_value(value);
+                let s = Schema {
+                    schema_data: Default::default(),
+                    schema_kind: SchemaKind::Type(ty),
+                };
+                let s = Box::new(s);
+                let s = ReferenceOr::Item(s);
 
-            if let Fields::Named(ref mut x) = s.fields {
-                for (key, value) in o.into_iter() {
-                    let ty = type_for(value, structs, name, count + 1);
+                properties.insert(key.to_string(), s);
+            }
 
-                    let key = format_ident!("{key}");
-
-                    x.named.push(
-                        syn::Field::parse_named
-                            .parse2(quote::quote! { pub #key: #ty })
-                            .unwrap(),
-                    );
-                }
-            } else {
-                panic!("This should always be named cause we just made the struct")
-            };
-
-            structs.push(s);
-
-            quote::quote!(#ident)
+            Type::Object(ObjectType {
+                properties: properties.into_iter().collect(),
+                required: vec![],
+                additional_properties: None,
+                min_properties: None,
+                max_properties: None,
+            })
         }
     }
+}
+
+fn type_for(
+    value: &Value,
+    new_structs: &mut Vec<ItemStruct>,
+    name: &str,
+    count: usize,
+) -> TokenStream {
+    let openapi_type = type_for_value(value);
+
+    into_type(&openapi_type, new_structs, name, count)
 }
 
 #[cfg(test)]
@@ -179,7 +323,7 @@ mod test {
 
     #[test]
     fn test_path_mod_names() {
-        let spec_string = include_str!("../../example_specs/some_site.json");
+        let spec_string = include_str!("../../usage-example/src/some_site.json");
         let spec: OpenAPI = serde_json::from_str(spec_string).unwrap();
 
         let paths = spec.paths;
--- usage-example/src/main.rs
+++ usage-example/src/main.rs
@@ -4,13 +4,16 @@ use openapi_gen_macro::api;
 mod some_site {}
 
 fn main() {
+    let _ = some_site::Test_More::Get::QueryParams {
+        test: "1".to_string(),
+    };
     let _ = some_site::Root::Get::Request {};
     let x = some_site::Root::Get::Response200 {
-        versions: vec![some_site::Root::Get::Response200_1 {
+        versions: vec![some_site::Root::Get::Response200_2 {
             updated: "1".to_string(),
             id: "1".to_string(),
             status: "200".to_string(),
-            links: vec![some_site::Root::Get::Response200_2 {
+            links: vec![some_site::Root::Get::Response200_4 {
                 href: "no".to_string(),
                 rel: "me".to_string(),
             }],
