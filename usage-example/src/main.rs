533136eb03386773468a6a90080f8844c17cc63a
Corey Alexander <coreyja@gmail.com>
2023-02-13 17:46:52 -0500

    Get the usage example working again

--- usage-example/src/main.rs
+++ usage-example/src/main.rs
@@ -4,13 +4,16 @@ use openapi_gen_macro::api;
 mod some_site {}
 
 fn main() {
-    let _ = some_site::test_more::get::request::QueryParams {
-        test: some_site::test_more::get::request::InnerParam {
+    let _ = some_site::test_more::post::request::Body {
+        foo: "bar".to_string(),
+        bar: "foo".to_string(),
+    };
+    let _ = some_site::test_more::post::request::QueryParams {
+        test: some_site::test_more::post::request::InnerParam {
             foo: "bar".to_string(),
             bar: "foo".to_string(),
         },
     };
-    let _ = some_site::root::get::request::Body {};
     // let x = some_site::root::get::response::Body200 {
     //     versions: vec![some_site::root::get::response::Body200_2 {
     //         updated: "1".to_string(),
