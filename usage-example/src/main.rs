518df8088f5cb71b076635f3fdfc486f3574b52e
Corey Alexander <coreyja@gmail.com>
2023-02-12 19:43:21 -0500

    Get usage example to compile

--- usage-example/src/main.rs
+++ usage-example/src/main.rs
@@ -4,25 +4,25 @@ use openapi_gen_macro::api;
 mod some_site {}
 
 fn main() {
-    let _ = some_site::Test_More::Get::QueryParams {
-        test: some_site::Test_More::Get::InnerParam {
+    let _ = some_site::test_more::get::request::QueryParams {
+        test: some_site::test_more::get::request::InnerParam {
             foo: "bar".to_string(),
             bar: "foo".to_string(),
         },
     };
-    let _ = some_site::Root::Get::Request {};
-    let x = some_site::Root::Get::Response200 {
-        versions: vec![some_site::Root::Get::Response200_2 {
+    let _ = some_site::root::get::request::Body {};
+    let x = some_site::root::get::response::Body200 {
+        versions: vec![some_site::root::get::response::Body200_2 {
             updated: "1".to_string(),
             id: "1".to_string(),
             status: "200".to_string(),
-            links: vec![some_site::Root::Get::Response200_4 {
+            links: vec![some_site::root::get::response::Body200_4 {
                 href: "no".to_string(),
                 rel: "me".to_string(),
             }],
         }],
     };
-    let _ = some_site::Root::Get::Response::_200(x);
+    let _ = some_site::root::get::response::Body::_200(x);
 
     println!("Hello, world!");
 }
