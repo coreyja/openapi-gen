4aa57deee083cb62ccfa469b96b9a6749b8074e1
Corey Alexander <coreyja@gmail.com>
2023-02-22 20:13:15 -0500

    Refactor to make clippy happy. We pulled out a struct that holds all the names for the structs we are creating

--- openapi-gen-core/src/response.rs
+++ openapi-gen-core/src/response.rs
@@ -3,6 +3,12 @@ use typify::{TypeSpace, TypeSpaceSettings};
 
 use super::*;
 
+struct ResponseStructNames {
+    variant_ident: syn::Ident,
+    struct_name: String,
+    header_name: String,
+}
+
 impl IntoMod for Responses {
     fn as_mod(&self, refs: &ReferenceableAPI) -> syn::ItemMod {
         let mut settings = TypeSpaceSettings::default();
@@ -25,30 +31,38 @@ impl IntoMod for Responses {
         for (status_code, resp) in &self.responses {
             let resp = refs.resolve(resp).unwrap();
 
+            let names = ResponseStructNames {
+                variant_ident: format_ident!("_{status_code}"),
+                struct_name: format!("Body{status_code}"),
+                header_name: format!("Headers{status_code}"),
+            };
+
             process_response(
                 resp,
                 refs,
                 &mut types,
                 &mut response_enum,
                 &mut headers_enum,
-                format_ident!("_{status_code}"),
-                format!("Headers{status_code}"),
-                format!("Body{status_code}"),
+                names,
             );
         }
 
         if let Some(resp) = &self.default {
             let resp = refs.resolve(resp).unwrap();
 
+            let names = ResponseStructNames {
+                variant_ident: format_ident!("Default"),
+                struct_name: "BodyDefault".to_string(),
+                header_name: "HeadersDefault".to_string(),
+            };
+
             process_response(
                 resp,
                 refs,
                 &mut types,
                 &mut response_enum,
                 &mut headers_enum,
-                format_ident!("Default"),
-                "DefaultHeaders".to_owned(),
-                "DefaultBody".to_owned(),
+                names,
             );
         }
 
@@ -77,25 +91,51 @@ fn process_response(
     types: &mut TypeSpace,
     response_enum: &mut ItemEnum,
     headers_enum: &mut ItemEnum,
-    variant_ident: syn::Ident,
-    header_name: String,
-    struct_name: String,
+    names: ResponseStructNames,
 ) {
-    // let variant_ident = format_ident!("_{status_code}");
+    let response_variant = to_response_variant(
+        &resp,
+        refs,
+        types,
+        names.variant_ident.clone(),
+        names.struct_name,
+    );
+    response_enum.variants.push(response_variant);
+
+    let header_variant =
+        to_header_variant(resp, refs, types, names.variant_ident, names.header_name);
+    if let Some(header_variant) = header_variant {
+        headers_enum.variants.push(header_variant);
+    }
+}
+
+fn to_response_variant(
+    resp: &Response,
+    refs: &ReferenceableAPI,
+    types: &mut TypeSpace,
+    variant_ident: syn::Ident,
+    struct_name: String,
+) -> syn::Variant {
     let content = &resp.content;
 
-    // let struct_ident = format!("Body{status_code}");
     let ty = content_to_tokens(refs, types, content, &struct_name);
 
     let desc = &resp.description;
-    let variant: syn::Variant = parse_quote! {
+    parse_quote! {
         #[doc = #desc]
         #variant_ident(#ty)
-    };
-    response_enum.variants.push(variant);
+    }
+}
 
+fn to_header_variant(
+    resp: Response,
+    refs: &ReferenceableAPI,
+    types: &mut TypeSpace,
+    variant_ident: syn::Ident,
+    header_name: String,
+) -> Option<syn::Variant> {
     let headers = &resp.headers;
-    if !headers.is_empty() {
+    let header_variant = if !headers.is_empty() {
         let schema = (refs, headers.clone()).to_schema();
         let type_id = types
             .add_type_with_name(&schema, Some(header_name))
@@ -103,10 +143,16 @@ fn process_response(
 
         let t = types.get_type(&type_id).unwrap();
         let t_ident = t.ident();
-        headers_enum.variants.push(parse_quote! {
+        let header_variant = parse_quote! {
             #variant_ident(#t_ident)
-        });
-    }
+        };
+
+        Some(header_variant)
+    } else {
+        None
+    };
+
+    header_variant
 }
 
 pub(crate) fn content_to_tokens(
